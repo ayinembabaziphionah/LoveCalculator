b712d27cec155e3a5be710a5e1474436e74b097d
ayinembabaziphionah <61048762+ayinembabaziphionah@users.noreply.github.com>
2020-11-13 18:56:47 +0300

    first commit

---
+++ app/src/main/java/com/example/lovecalculator/Calculator.kt
@@ -0,0 +1,11 @@
+package com.example.lovecalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Calculator : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calculator)
+    }
+}
---
+++ app/src/main/java/com/example/lovecalculator/Network/ApiClient.kt
@@ -0,0 +1,4 @@
+package com.example.lovecalculator.Network
+
+class ApiClient {
+}
